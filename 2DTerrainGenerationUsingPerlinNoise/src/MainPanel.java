7973acdb7616275dae99521b658a05624079471e
BrokenWrench2003 <43032452+BrokenWrench2003@users.noreply.github.com>
2021-05-20 13:23:59 +0200

    Created Display,  Created 2D Perlin Class
    Need to fix BufferedImage RGB parameters

---
+++ 2DTerrainGenerationUsingPerlinNoise/src/MainPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+public class MainPanel extends JPanel{
+	private Timer timer = new Timer(30, e -> repaint());
+	public MainPanel(){
+		timer.start();
+	}
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage(PerlinNoise2D.getNoiseImage(), 0, 0, this);
+	}
+}
